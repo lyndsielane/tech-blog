e5ab8bab51a9d2878917112ff22c8b6b6b78a6a1
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-31 20:08:49 -0400

    login form and js connected

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,3 +1,27 @@
+//Login Functions
+const loginFormFunction = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#log-email').value.trim();
+    const password = document.querySelector('#log-password').value.trim();
+
+    if (email && password) {
+
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+//Login Form Script 
 $(function () {
   var showClass = "show";
 
@@ -14,3 +38,9 @@ $(function () {
       $(this).trigger("checkval");
     });
 });
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormFunction);
+
+//Signup Functions
